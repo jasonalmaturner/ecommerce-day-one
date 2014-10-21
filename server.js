46eab357670b00613cc122d04a52ec885c7ebefa
jasonalmaturner <jasonalmaturner@gmail.com>
2014-10-20 19:30:34 -0600

    working on schemas

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express'),
+		app = express(),
+		mongoose = require('mongoose');
+
+
+
+mongoose.connect('mongodb://localhost/eCommerce');
+
+app.listen(9008);
