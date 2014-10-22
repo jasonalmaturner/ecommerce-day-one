62bc1a77771a27a612f61332c971742866b68891
jasonalmaturner <jasonalmaturner@gmail.com>
2014-10-22 12:32:08 -0600

    Setting up API routes

--- migrations/001-seed-customers.js
+++ migrations/001-seed-customers.js
@@ -5,23 +5,28 @@ require('../server')
 
 exports.up = function(next){
 	var jason = new Customer({
-	name: "jason",
-	email: "jasonalmaturner@gmail.com",
-	shippingAddress: "122 South 350 East Burley, ID 83318",
-	billingAddress: "122 South 350 East Burley, ID 83318",
-	phoneNumber: 2086500116,
-	password: 987654321,
-	active: true
-})
+		name: {
+			first: 'Jason',
+			middle: 'Alma',
+			last: 'Turner'
+		}
+		email: "jasonalmaturner@gmail.com",
+		shippingAddress: "122 South 350 East Burley, ID 83318",
+		billingAddress: "122 South 350 East Burley, ID 83318",
+		phoneNumber: 2086500116,
+		password: 987654321,
+		active: true
+	});
 
 jason.save(function(err){
 	if(err){
 		console.log(err);
+		next(err);
 	} else {
-		console.log('jason was saved')
+		console.log('jason was saved');
+		next();
 	}
 })
-	next();
 };
 
 exports.down = function(next){
--- server.js
+++ server.js
@@ -2,20 +2,42 @@
 
 var express = require('express'),
 	app = express(),
-	mongoose = require('mongoose');
+	mongoose = require('mongoose'),
+	bodyParser = require('body-parser');
 
-var Customer = require('./lib/customer'),
-	Order	 = require('./lib/order'),
-	Product  = require('./lib/product');
+var Customer = require('./lib/models/customer'),
+	Order	 = require('./lib/models/order'),
+	Product  = require('./lib/models/product'),
+	ProductController = require('./lib/controllers/product-controller'),
+	CustomerController = require('./lib/controllers/customer-controller');
 
+//middleware
+app.use(bodyParser.json());
+
+//Mongoose Connection
 var mongoUrl = 'mongodb://localhost/eCommerce';
-
-// mongoose.connect(mongoUrl);
-
-// app.listen(9008);
+mongoose.connect(mongoUrl);
 
 var connection = mongoose.connection;
 connection.once('open', function (){
 	console.log('Successfully connected to ' + mongoUrl)
 })
 global.mongooseConnection = connection;
+
+//server startup
+app.listen(9008, function(){
+	console.log('Express listening on port 9008')
+});
+
+//routes
+app.get('/products', ProductController.get);
+
+app.post('/products', ProductController.post);
+
+app.put('/products/:id', ProductController.put);
+
+app.get('/customers', CustomerController.get);
+
+app.post('/customers', CustomerController.post);
+
+app.put('/customers/:id', CustomerController.put);
